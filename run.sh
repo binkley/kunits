f70e206c10e327a49a7ab377be5a28fb179cb69c
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-03-23 06:03:37 -0500

    Fix run

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-./mvnw "$@" &&
-    exec java -jar target/kotlin-rational-0-SNAPSHOT-jar-with-dependencies.jar
+test -r target/kunit-0-SNAPSHOT-jar-with-dependencies.jar || ./mvnw "$@"
+
+exec java -jar target/kunit-0-SNAPSHOT-jar-with-dependencies.jar
