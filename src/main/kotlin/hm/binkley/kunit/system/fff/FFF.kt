aae15b62b91bedd929f0451ec2afe17fa2af9b2b
Brian Oxley <boxley@thoughtworks.com>
2020-03-25 15:02:44 -0500

    More specific

--- src/main/kotlin/hm/binkley/kunit/system/fff/FFF.kt
+++ src/main/kotlin/hm/binkley/kunit/system/fff/FFF.kt
@@ -1,8 +1,8 @@
 package hm.binkley.kunit.system.fff
 
+import hm.binkley.kunit.Lengths
 import hm.binkley.kunit.Measure
 import hm.binkley.kunit.System
-import hm.binkley.kunit.Units
 import hm.binkley.kunit.system.english.English
 import hm.binkley.kunit.system.english.length.Yards
 import hm.binkley.kunit.system.fff.length.Furlongs
@@ -10,6 +10,6 @@ import hm.binkley.math.finite.times
 
 object FFF : System<FFF>("FFF")
 
-fun <U : Units<FFF, U>, V : Units<English, V>> Measure<FFF, U>.into(
+fun <U : Lengths<FFF, U>, V : Lengths<English, V>> Measure<FFF, U>.into(
     other: V
 ) = Yards.new(to(Furlongs).value * 220).to(other)
