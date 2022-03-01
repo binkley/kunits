b31889a87c66e6002e20daffaef86a708d6f31fa
Brian Oxley <boxley@thoughtworks.com>
2022-03-01 11:28:46 -0600

    Readability and consistency

--- src/test/kotlin/hm/binkley/kunits/system/english/length/EnglishLengthsTest.kt
+++ src/test/kotlin/hm/binkley/kunits/system/english/length/EnglishLengthsTest.kt
@@ -8,74 +8,74 @@ internal class EnglishLengthsTest {
     @Test
     fun `should convert`() {
         1L.poppyseeds shouldBe 1.poppyseeds
-        1.poppyseeds.barleycorns shouldBe (1 over 4).barleycorns
-        1.poppyseeds.inches shouldBe (1 over 12).inches
-        1.poppyseeds.sticks shouldBe (1 over 24).sticks
-        1.poppyseeds.hands shouldBe (1 over 48).hands
-        1.poppyseeds.feet shouldBe (1 over 144).feet
-        1.poppyseeds.yards shouldBe (1 over 432).yards
-        1.poppyseeds.ells shouldBe (1 over 540).ells
-        1.poppyseeds.rods shouldBe (1 over 2_376).rods
-        1.poppyseeds.chains shouldBe (1 over 9_504).chains
-        1.poppyseeds.furlongs shouldBe (1 over 95_040).furlongs
-        1.poppyseeds.miles shouldBe (1 over 760_320).miles
-        1.poppyseeds.leagues shouldBe (1 over 2_280_960).leagues
+        (1 over 4).barleycorns shouldBe 1.poppyseeds.barleycorns
+        (1 over 12).inches shouldBe 1.poppyseeds.inches
+        (1 over 24).sticks shouldBe 1.poppyseeds.sticks
+        (1 over 48).hands shouldBe 1.poppyseeds.hands
+        (1 over 144).feet shouldBe 1.poppyseeds.feet
+        (1 over 432).yards shouldBe 1.poppyseeds.yards
+        (1 over 540).ells shouldBe 1.poppyseeds.ells
+        (1 over 2_376).rods shouldBe 1.poppyseeds.rods
+        (1 over 9_504).chains shouldBe 1.poppyseeds.chains
+        (1 over 95_040).furlongs shouldBe 1.poppyseeds.furlongs
+        (1 over 760_320).miles shouldBe 1.poppyseeds.miles
+        (1 over 2_280_960).leagues shouldBe 1.poppyseeds.leagues
 
         4.poppyseeds shouldBe 1.barleycorns.poppyseeds
         1L.barleycorns shouldBe 1.barleycorns
-        1.barleycorns.inches shouldBe (1 over 3).inches
-        1.barleycorns.sticks shouldBe (1 over 6).sticks
-        1.barleycorns.hands shouldBe (1 over 12).hands
-        1.barleycorns.feet shouldBe (1 over 36).feet
-        1.barleycorns.yards shouldBe (1 over 108).yards
-        1.barleycorns.ells shouldBe (1 over 135).ells
-        1.barleycorns.rods shouldBe (1 over 594).rods
-        1.barleycorns.chains shouldBe (1 over 2_376).chains
-        1.barleycorns.furlongs shouldBe (1 over 23_760).furlongs
-        1.barleycorns.miles shouldBe (1 over 190_080).miles
-        1.barleycorns.leagues shouldBe (1 over 570_240).leagues
+        (1 over 3).inches shouldBe 1.barleycorns.inches
+        (1 over 6).sticks shouldBe 1.barleycorns.sticks
+        (1 over 12).hands shouldBe 1.barleycorns.hands
+        (1 over 36).feet shouldBe 1.barleycorns.feet
+        (1 over 108).yards shouldBe 1.barleycorns.yards
+        (1 over 135).ells shouldBe 1.barleycorns.ells
+        (1 over 594).rods shouldBe 1.barleycorns.rods
+        (1 over 2_376).chains shouldBe 1.barleycorns.chains
+        (1 over 23_760).furlongs shouldBe 1.barleycorns.furlongs
+        (1 over 190_080).miles shouldBe 1.barleycorns.miles
+        (1 over 570_240).leagues shouldBe 1.barleycorns.leagues
 
         12.poppyseeds shouldBe 1.inches.poppyseeds
         3.barleycorns shouldBe 1.inches.barleycorns
         1L.inches shouldBe 1.inches
-        1.inches.sticks shouldBe (1 over 2).sticks
-        1.inches.hands shouldBe (1 over 4).hands
-        1.inches.feet shouldBe (1 over 12).feet
-        1.inches.yards shouldBe (1 over 36).yards
-        1.inches.ells shouldBe (1 over 45).ells
-        1.inches.rods shouldBe (1 over 198).rods
-        1.inches.chains shouldBe (1 over 792).chains
-        1.inches.furlongs shouldBe (1 over 7_920).furlongs
-        1.inches.miles shouldBe (1 over 63_360).miles
-        1.inches.leagues shouldBe (1 over 190_080).leagues
+        (1 over 2).sticks shouldBe 1.inches.sticks
+        (1 over 4).hands shouldBe 1.inches.hands
+        (1 over 12).feet shouldBe 1.inches.feet
+        (1 over 36).yards shouldBe 1.inches.yards
+        (1 over 45).ells shouldBe 1.inches.ells
+        (1 over 198).rods shouldBe 1.inches.rods
+        (1 over 792).chains shouldBe 1.inches.chains
+        (1 over 7_920).furlongs shouldBe 1.inches.furlongs
+        (1 over 63_360).miles shouldBe 1.inches.miles
+        (1 over 190_080).leagues shouldBe 1.inches.leagues
 
         24.poppyseeds shouldBe 1.sticks.poppyseeds
         6.barleycorns shouldBe 1.sticks.barleycorns
         2.inches shouldBe 1.sticks.inches
         1L.sticks shouldBe 1.sticks
-        1.sticks.hands shouldBe (1 over 2).hands
-        1.sticks.feet shouldBe (1 over 6).feet
-        1.sticks.yards shouldBe (1 over 18).yards
-        1.sticks.ells shouldBe (2 over 45).ells
-        1.sticks.rods shouldBe (1 over 99).rods
-        1.sticks.chains shouldBe (1 over 396).chains
-        1.sticks.furlongs shouldBe (1 over 3_960).furlongs
-        1.sticks.miles shouldBe (1 over 31_680).miles
-        1.sticks.leagues shouldBe (1 over 95_040).leagues
+        (1 over 2).hands shouldBe 1.sticks.hands
+        (1 over 6).feet shouldBe 1.sticks.feet
+        (1 over 18).yards shouldBe 1.sticks.yards
+        (2 over 45).ells shouldBe 1.sticks.ells
+        (1 over 99).rods shouldBe 1.sticks.rods
+        (1 over 396).chains shouldBe 1.sticks.chains
+        (1 over 3_960).furlongs shouldBe 1.sticks.furlongs
+        (1 over 31_680).miles shouldBe 1.sticks.miles
+        (1 over 95_040).leagues shouldBe 1.sticks.leagues
 
         48.poppyseeds shouldBe 1.hands.poppyseeds
         12.barleycorns shouldBe 1.hands.barleycorns
         4.inches shouldBe 1.hands.inches
         2.sticks shouldBe 1.hands.sticks
         1L.hands shouldBe 1.hands
-        1.hands.feet shouldBe (1 over 3).feet
-        1.hands.yards shouldBe (1 over 9).yards
-        1.hands.ells shouldBe (4 over 45).ells
-        1.hands.rods shouldBe (2 over 99).rods
-        1.hands.chains shouldBe (1 over 198).chains
-        1.hands.furlongs shouldBe (1 over 1_980).furlongs
-        1.hands.miles shouldBe (1 over 15_840).miles
-        1.hands.leagues shouldBe (1 over 47_520).leagues
+        (1 over 3).feet shouldBe 1.hands.feet
+        (1 over 9).yards shouldBe 1.hands.yards
+        (4 over 45).ells shouldBe 1.hands.ells
+        (2 over 99).rods shouldBe 1.hands.rods
+        (1 over 198).chains shouldBe 1.hands.chains
+        (1 over 1_980).furlongs shouldBe 1.hands.furlongs
+        (1 over 15_840).miles shouldBe 1.hands.miles
+        (1 over 47_520).leagues shouldBe 1.hands.leagues
 
         144.poppyseeds shouldBe 1.feet.poppyseeds
         36.barleycorns shouldBe 1.feet.barleycorns
@@ -83,13 +83,13 @@ internal class EnglishLengthsTest {
         6.sticks shouldBe 1.feet.sticks
         3.hands shouldBe 1.feet.hands
         1L.feet shouldBe 1.feet
-        1.feet.yards shouldBe (1 over 3).yards
-        1.feet.ells shouldBe (4 over 15).ells
-        1.feet.rods shouldBe (2 over 33).rods
-        1.feet.chains shouldBe (1 over 66).chains
-        1.feet.furlongs shouldBe (1 over 660).furlongs
-        1.feet.miles shouldBe (1 over 5_280).miles
-        1.feet.leagues shouldBe (1 over 15_840).leagues
+        (1 over 3).yards shouldBe 1.feet.yards
+        (4 over 15).ells shouldBe 1.feet.ells
+        (2 over 33).rods shouldBe 1.feet.rods
+        (1 over 66).chains shouldBe 1.feet.chains
+        (1 over 660).furlongs shouldBe 1.feet.furlongs
+        (1 over 5_280).miles shouldBe 1.feet.miles
+        (1 over 15_840).leagues shouldBe 1.feet.leagues
 
         432.poppyseeds shouldBe 1.yards.poppyseeds
         108.barleycorns shouldBe 1.yards.barleycorns
@@ -98,12 +98,12 @@ internal class EnglishLengthsTest {
         9.hands shouldBe 1.yards.hands
         3.feet shouldBe 1.yards.feet
         1L.yards shouldBe 1.yards
-        1.yards.ells shouldBe (4 over 5).ells
-        1.yards.rods shouldBe (2 over 11).rods
-        1.yards.furlongs shouldBe (1 over 220).furlongs
-        1.yards.chains shouldBe (1 over 22).chains
-        1.yards.miles shouldBe (1 over 1_760).miles
-        1.yards.leagues shouldBe (1 over 5_280).leagues
+        (4 over 5).ells shouldBe 1.yards.ells
+        (2 over 11).rods shouldBe 1.yards.rods
+        (1 over 220).furlongs shouldBe 1.yards.furlongs
+        (1 over 22).chains shouldBe 1.yards.chains
+        (1 over 1_760).miles shouldBe 1.yards.miles
+        (1 over 5_280).leagues shouldBe 1.yards.leagues
 
         540.poppyseeds shouldBe 1.ells.poppyseeds
         135.barleycorns shouldBe 1.ells.barleycorns
@@ -113,11 +113,11 @@ internal class EnglishLengthsTest {
         (15 over 4).feet shouldBe 1.ells.feet
         (5 over 4).yards shouldBe 1.ells.yards
         1L.ells shouldBe 1.ells
-        1.ells.rods shouldBe (5 over 22).rods
-        1.ells.furlongs shouldBe (1 over 176).furlongs
-        1.ells.chains shouldBe (5 over 88).chains
-        1.ells.miles shouldBe (1 over 1_408).miles
-        1.ells.leagues shouldBe (1 over 4_224).leagues
+        (5 over 22).rods shouldBe 1.ells.rods
+        (1 over 176).furlongs shouldBe 1.ells.furlongs
+        (5 over 88).chains shouldBe 1.ells.chains
+        (1 over 1_408).miles shouldBe 1.ells.miles
+        (1 over 4_224).leagues shouldBe 1.ells.leagues
 
         2_376.poppyseeds shouldBe 1.rods.poppyseeds
         594.barleycorns shouldBe 1.rods.barleycorns
@@ -128,10 +128,10 @@ internal class EnglishLengthsTest {
         (11 over 2).yards shouldBe 1.rods.yards
         (22 over 5).ells shouldBe 1.rods.ells
         1L.rods shouldBe 1.rods
-        1.rods.chains shouldBe (1 over 4).chains
-        1.rods.furlongs shouldBe (1 over 40).furlongs
-        1.rods.miles shouldBe (1 over 320).miles
-        1.rods.leagues shouldBe (1 over 960).leagues
+        (1 over 4).chains shouldBe 1.rods.chains
+        (1 over 40).furlongs shouldBe 1.rods.furlongs
+        (1 over 320).miles shouldBe 1.rods.miles
+        (1 over 960).leagues shouldBe 1.rods.leagues
 
         9_504.poppyseeds shouldBe 1.chains.poppyseeds
         2_376.barleycorns shouldBe 1.chains.barleycorns
@@ -143,9 +143,9 @@ internal class EnglishLengthsTest {
         (88 over 5).ells shouldBe 1.chains.ells
         4.rods shouldBe 1.chains.rods
         1L.chains shouldBe 1.chains
-        1.chains.furlongs shouldBe (1 over 10).furlongs
-        1.chains.miles shouldBe (1 over 80).miles
-        1.chains.leagues shouldBe (1 over 240).leagues
+        (1 over 10).furlongs shouldBe 1.chains.furlongs
+        (1 over 80).miles shouldBe 1.chains.miles
+        (1 over 240).leagues shouldBe 1.chains.leagues
 
         95_040.poppyseeds shouldBe 1.furlongs.poppyseeds
         23_760.barleycorns shouldBe 1.furlongs.barleycorns
@@ -158,8 +158,8 @@ internal class EnglishLengthsTest {
         40.rods shouldBe 1.furlongs.rods
         10.chains shouldBe 1.furlongs.chains
         1L.furlongs shouldBe 1.furlongs
-        1.furlongs.miles shouldBe (1 over 8).miles
-        1.furlongs.leagues shouldBe (1 over 24).leagues
+        (1 over 8).miles shouldBe 1.furlongs.miles
+        (1 over 24).leagues shouldBe 1.furlongs.leagues
 
         760_320.poppyseeds shouldBe 1.miles.poppyseeds
         190_080.barleycorns shouldBe 1.miles.barleycorns
@@ -173,7 +173,7 @@ internal class EnglishLengthsTest {
         80.chains shouldBe 1.miles.chains
         8.furlongs shouldBe 1.miles.furlongs
         1L.miles shouldBe 1.miles
-        1.miles.leagues shouldBe (1 over 3).leagues
+        (1 over 3).leagues shouldBe 1.miles.leagues
 
         2_280_960.poppyseeds shouldBe 1.leagues.poppyseeds
         570_240.barleycorns shouldBe 1.leagues.barleycorns
--- src/test/kotlin/hm/binkley/kunits/system/english/weight/EnglishWeightsTest.kt
+++ src/test/kotlin/hm/binkley/kunits/system/english/weight/EnglishWeightsTest.kt
@@ -8,54 +8,54 @@ internal class EnglishWeightsTest {
     @Test
     fun `should convert`() {
         1L.grains shouldBe 1.grains
-        1.grains.drams shouldBe (32 over 875).drams
-        1.grains.ounces shouldBe (2 over 875).ounces
-        1.grains.pounds shouldBe (1 over 7000).pounds
-        1.grains.cloves shouldBe (1 over 49_000).cloves
-        1.grains.stone shouldBe (1 over 98_000).stone
-        1.grains.tods shouldBe (1 over 196_000).tods
-        1.grains.hundredweight shouldBe (1 over 784_000).hundredweight
-        1.grains.tuns shouldBe (1 over 15_680_000).tuns
+        (32 over 875).drams shouldBe 1.grains.drams
+        (2 over 875).ounces shouldBe 1.grains.ounces
+        (1 over 7000).pounds shouldBe 1.grains.pounds
+        (1 over 49_000).cloves shouldBe 1.grains.cloves
+        (1 over 98_000).stone shouldBe 1.grains.stone
+        (1 over 196_000).tods shouldBe 1.grains.tods
+        (1 over 784_000).hundredweight shouldBe 1.grains.hundredweight
+        (1 over 15_680_000).tuns shouldBe 1.grains.tuns
 
         (875 over 32).grains shouldBe 1.drams.grains
         1L.drams shouldBe 1.drams
-        1.drams.ounces shouldBe (1 over 16).ounces
-        1.drams.pounds shouldBe (1 over 256).pounds
-        1.drams.cloves shouldBe (1 over 1_792).cloves
-        1.drams.stone shouldBe (1 over 3_584).stone
-        1.drams.tods shouldBe (1 over 7_168).tods
-        1.drams.hundredweight shouldBe (1 over 28_672).hundredweight
-        1.drams.tuns shouldBe (1 over 573_440).tuns
+        (1 over 16).ounces shouldBe 1.drams.ounces
+        (1 over 256).pounds shouldBe 1.drams.pounds
+        (1 over 1_792).cloves shouldBe 1.drams.cloves
+        (1 over 3_584).stone shouldBe 1.drams.stone
+        (1 over 7_168).tods shouldBe 1.drams.tods
+        (1 over 28_672).hundredweight shouldBe 1.drams.hundredweight
+        (1 over 573_440).tuns shouldBe 1.drams.tuns
 
         (875 over 2).grains shouldBe 1.ounces.grains
         16.drams shouldBe 1.ounces.drams
         1L.ounces shouldBe 1.ounces
-        1.ounces.pounds shouldBe (1 over 16).pounds
-        1.ounces.cloves shouldBe (1 over 112).cloves
-        1.ounces.stone shouldBe (1 over 224).stone
-        1.ounces.tods shouldBe (1 over 448).tods
-        1.ounces.hundredweight shouldBe (1 over 1_792).hundredweight
-        1.ounces.tuns shouldBe (1 over 35_840).tuns
+        (1 over 16).pounds shouldBe 1.ounces.pounds
+        (1 over 112).cloves shouldBe 1.ounces.cloves
+        (1 over 224).stone shouldBe 1.ounces.stone
+        (1 over 448).tods shouldBe 1.ounces.tods
+        (1 over 1_792).hundredweight shouldBe 1.ounces.hundredweight
+        (1 over 35_840).tuns shouldBe 1.ounces.tuns
 
         7000.grains shouldBe 1.pounds.grains
         256.drams shouldBe 1.pounds.drams
         16.ounces shouldBe 1.pounds.ounces
         1L.pounds shouldBe 1.pounds
-        1.pounds.cloves shouldBe (1 over 7).cloves
-        1.pounds.stone shouldBe (1 over 14).stone
-        1.pounds.tods shouldBe (1 over 28).tods
-        1.pounds.hundredweight shouldBe (1 over 112).hundredweight
-        1.pounds.tuns shouldBe (1 over 2_240).tuns
+        (1 over 7).cloves shouldBe 1.pounds.cloves
+        (1 over 14).stone shouldBe 1.pounds.stone
+        (1 over 28).tods shouldBe 1.pounds.tods
+        (1 over 112).hundredweight shouldBe 1.pounds.hundredweight
+        (1 over 2_240).tuns shouldBe 1.pounds.tuns
 
         49_000.grains shouldBe 1.cloves.grains
         1_792.drams shouldBe 1.cloves.drams
         112.ounces shouldBe 1.cloves.ounces
         7.pounds shouldBe 1.cloves.pounds
         1L.cloves shouldBe 1.cloves
-        1.cloves.stone shouldBe (1 over 2).stone
-        1.cloves.tods shouldBe (1 over 4).tods
-        1.cloves.hundredweight shouldBe (1 over 16).hundredweight
-        1.cloves.tuns shouldBe (1 over 320).tuns
+        (1 over 2).stone shouldBe 1.cloves.stone
+        (1 over 4).tods shouldBe 1.cloves.tods
+        (1 over 16).hundredweight shouldBe 1.cloves.hundredweight
+        (1 over 320).tuns shouldBe 1.cloves.tuns
 
         98_000.grains shouldBe 1.stone.grains
         3_584.drams shouldBe 1.stone.drams
@@ -63,9 +63,9 @@ internal class EnglishWeightsTest {
         14.pounds shouldBe 1.stone.pounds
         2.cloves shouldBe 1.stone.cloves
         1L.stone shouldBe 1.stone
-        1.stone.tods shouldBe (1 over 2).tods
-        1.stone.hundredweight shouldBe (1 over 8).hundredweight
-        1.stone.tuns shouldBe (1 over 160).tuns
+        (1 over 2).tods shouldBe 1.stone.tods
+        (1 over 8).hundredweight shouldBe 1.stone.hundredweight
+        (1 over 160).tuns shouldBe 1.stone.tuns
 
         196_000.grains shouldBe 1.tods.grains
         7_168.drams shouldBe 1.tods.drams
@@ -74,8 +74,8 @@ internal class EnglishWeightsTest {
         4.cloves shouldBe 1.tods.cloves
         2.stone shouldBe 1.tods.stone
         1L.tods shouldBe 1.tods
-        1.tods.hundredweight shouldBe (1 over 4).hundredweight
-        1.tods.tuns shouldBe (1 over 80).tuns
+        (1 over 4).hundredweight shouldBe 1.tods.hundredweight
+        (1 over 80).tuns shouldBe 1.tods.tuns
 
         784_000.grains shouldBe 1.hundredweight.grains
         28_672.drams shouldBe 1.hundredweight.drams
@@ -85,7 +85,7 @@ internal class EnglishWeightsTest {
         8.stone shouldBe 1.hundredweight.stone
         4.tods shouldBe 1.hundredweight.tods
         1L.hundredweight shouldBe 1.hundredweight
-        1.hundredweight.tuns shouldBe (1 over 20).tuns
+        (1 over 20).tuns shouldBe 1.hundredweight.tuns
 
         15_680_000.grains shouldBe 1.tuns.grains
         573_440.drams shouldBe 1.tuns.drams
