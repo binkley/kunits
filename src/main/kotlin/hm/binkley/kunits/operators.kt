c9d999ed116bab37be0c47a00882961b9370c087
Brian Oxley <boxley@thoughtworks.com>
2022-03-02 14:35:08 -0600

    Docs, hygiene

--- src/main/kotlin/hm/binkley/kunits/operators.kt
+++ src/main/kotlin/hm/binkley/kunits/operators.kt
@@ -5,63 +5,56 @@ import hm.binkley.math.fixed.FixedBigRational
 import hm.binkley.math.times
 
 /** Returns this measure. */
-operator fun <S : System<S>, U : Units<S, U>> Measure<S, U>.unaryPlus() = this
+operator fun <S : System<S>, U : Units<S, U>>
+Measure<S, U>.unaryPlus() = this
 
 /** Returns the additive inverse of this measure. */
-operator fun <S : System<S>, U : Units<S, U>> Measure<S, U>.unaryMinus() =
-    unit.new(-value)
+operator fun <S : System<S>, U : Units<S, U>>
+Measure<S, U>.unaryMinus() = unit.new(-value)
 
-/** Adds the measures, with the _left_ returned as units. */
-operator fun <S : System<S>, U : Units<S, U>, V : Units<S, V>> Measure<S, U>.plus(
-    other: Measure<S, V>,
-): Measure<S, U> = unit.new(value + other.into(unit).value)
+/** Adds the two measures using _left_ measure for units. */
+operator fun <S : System<S>, U : Units<S, U>, V : Units<S, V>>
+Measure<S, U>.plus(other: Measure<S, V>) =
+    unit.new(value + other.into(unit).value)
 
-/** Subtracts the measures, with the _left_ returned as units. */
-operator fun <S : System<S>, U : Units<S, U>, V : Units<S, V>> Measure<S, U>.minus(
-    other: Measure<S, V>,
-): Measure<S, U> = unit.new(value - other.into(unit).value)
+/** Subtracts the two measures using _left_ measure for units. */
+operator fun <S : System<S>, U : Units<S, U>, V : Units<S, V>>
+Measure<S, U>.minus(other: Measure<S, V>) =
+    unit.new(value - other.into(unit).value)
 
-/** Scales up the measure, with the _left_ returned as units. */
-operator fun <S : System<S>, U : Units<S, U>> Measure<S, U>.times(
-    multiplicand: FixedBigRational,
-) = unit.new(value * multiplicand)
+/** Scales up the measure. */
+operator fun <S : System<S>, U : Units<S, U>>
+Measure<S, U>.times(factor: FixedBigRational) = unit.new(value * factor)
 
-/** Scales up the measure, with the _left_ returned as units. */
-operator fun <S : System<S>, U : Units<S, U>> FixedBigRational.times(
-    multiplicand: Measure<S, U>,
-) = multiplicand.unit.new(this * multiplicand.value)
+/** Scales up the measure. */
+operator fun <S : System<S>, U : Units<S, U>>
+FixedBigRational.times(factor: Measure<S, U>) =
+    factor.unit.new(this * factor.value)
 
-/** Scales up the measure, with the _left_ returned as units. */
-operator fun <S : System<S>, U : Units<S, U>> Measure<S, U>.times(
-    multiplicand: Long,
-) = unit.new(value * multiplicand)
+/** Scales up the measure. */
+operator fun <S : System<S>, U : Units<S, U>>
+Measure<S, U>.times(factor: Long) = unit.new(value * factor)
 
-/** Scales up the measure, with the _right_ returned as units. */
-operator fun <S : System<S>, U : Units<S, U>> Long.times(
-    multiplicand: Measure<S, U>,
-) = multiplicand.unit.new(this * multiplicand.value)
+/** Scales up the measure. */
+operator fun <S : System<S>, U : Units<S, U>>
+Long.times(factor: Measure<S, U>) = factor.unit.new(this * factor.value)
 
-/** Scales up the measure, with the _left_ returned as units. */
-operator fun <S : System<S>, U : Units<S, U>> Measure<S, U>.times(
-    multiplicand: Int,
-) = unit.new(value * multiplicand)
+/** Scales up the measure. */
+operator fun <S : System<S>, U : Units<S, U>>
+Measure<S, U>.times(factor: Int) = unit.new(value * factor)
 
-/** Scales down the measure, with the _left_ returned as units. */
-operator fun <S : System<S>, U : Units<S, U>> Measure<S, U>.div(
-    divisor: FixedBigRational,
-) = unit.new(value / divisor)
+/** Scales up the measure. */
+operator fun <S : System<S>, U : Units<S, U>>
+Int.times(factor: Measure<S, U>) = factor.unit.new(this * factor.value)
 
-/** Scales down the measure, with the _left_ returned as units. */
-operator fun <S : System<S>, U : Units<S, U>> Measure<S, U>.div(
-    divisor: Long,
-) = unit.new(value / divisor)
+/** Scales down the measure. */
+operator fun <S : System<S>, U : Units<S, U>>
+Measure<S, U>.div(factor: FixedBigRational) = unit.new(value / factor)
 
-/** Scales up the measure, with the _right_ returned as units. */
-operator fun <S : System<S>, U : Units<S, U>> Int.times(
-    multiplicand: Measure<S, U>,
-) = multiplicand.unit.new(this * multiplicand.value)
+/** Scales down the measure. */
+operator fun <S : System<S>, U : Units<S, U>>
+Measure<S, U>.div(factor: Long) = unit.new(value / factor)
 
-/** Scales down the measure, with the _left_ returned as units. */
-operator fun <S : System<S>, U : Units<S, U>> Measure<S, U>.div(
-    divisor: Int,
-) = unit.new(value / divisor)
+/** Scales down the measure. */
+operator fun <S : System<S>, U : Units<S, U>>
+Measure<S, U>.div(factor: Int) = unit.new(value / factor)
