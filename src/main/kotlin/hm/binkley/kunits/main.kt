44a09d36020a0e30b7e099b54ca471c7c9c159a1
Brian Oxley <boxley@thoughtworks.com>
2022-03-02 08:19:15 -0600

    Better demo

--- src/main/kotlin/hm/binkley/kunits/main.kt
+++ src/main/kotlin/hm/binkley/kunits/main.kt
@@ -1,5 +1,8 @@
 package hm.binkley.kunits
 
+import hm.binkley.kunits.system.english.English
+import hm.binkley.kunits.system.english.length.Inches
+import hm.binkley.kunits.system.english.length.Yards
 import hm.binkley.kunits.system.english.length.barleycorns
 import hm.binkley.kunits.system.english.length.feet
 import hm.binkley.kunits.system.english.length.hands
@@ -7,10 +10,18 @@ import hm.binkley.kunits.system.english.length.inches
 import hm.binkley.kunits.system.english.length.lines
 import hm.binkley.kunits.system.english.length.sticks
 import hm.binkley.kunits.system.english.length.yards
+import hm.binkley.kunits.system.english.toFFF
 import hm.binkley.kunits.system.english.weight.drams
 import hm.binkley.kunits.system.english.weight.ounces
 import hm.binkley.kunits.system.english.weight.pounds
 import hm.binkley.kunits.system.english.weight.stone
+import hm.binkley.kunits.system.fff.FFF
+import hm.binkley.kunits.system.fff.length.Furlongs
+import hm.binkley.kunits.system.fff.toEnglish
+import hm.binkley.kunits.system.mit.MIT
+import hm.binkley.kunits.system.mit.toEnglish
+import hm.binkley.kunits.system.fff.length.furlongs as fffFurlongs
+import hm.binkley.kunits.system.mit.length.smoots as mitSmoots
 
 /** Runs the demo. */
 fun main() {
@@ -44,4 +55,10 @@ fun main() {
     println(m2 / 4)
     println(m2.pounds.stone)
     println((m2.stone + m2.pounds - m2.ounces).drams)
+
+    println()
+    println("== CONVERSIONS")
+    println("${1.fffFurlongs} IN $FFF IS ${1.fffFurlongs.toEnglish(Yards)} IN $English")
+    println("${220.yards} IN $English IS ${220.yards.toFFF(Furlongs)} IN $FFF")
+    println("${1.mitSmoots} IN $MIT IS ${1.mitSmoots.toEnglish(Inches)} IN $English")
 }
--- src/test/kotlin/hm/binkley/kunits/MainTest.kt
+++ src/test/kotlin/hm/binkley/kunits/MainTest.kt
@@ -1,13 +1,46 @@
 package hm.binkley.kunits
 
-import com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut
+import com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemErrAndOutNormalized
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 internal class MainTest {
     @Test
     fun `should run`() {
-        tapSystemOut {
-            main()
-        }
+        tapSystemErrAndOutNormalized { main() } shouldBeAfterTrimming """
+== ENGLISH LENGTHS
+120 lines
+-120 lines
+120 lines
+30 Bc
+240 lines
+240 lines
+0 hh
+360 lines
+40 lines
+5 sticks
+5⁄2 hh
+
+== AVOIRDUPOIS WEIGHTS
+300 dr
+-300 dr
+300 dr
+75⁄4 oz
+600 dr
+316 dr
+0 lb
+1200 dr
+75 dr
+75⁄896 st
+300 dr
+
+== CONVERSIONS
+1 fur IN FFF IS 220 yd IN English
+220 yd IN English IS 1 fur IN FFF
+1 sm IN MIT IS 67" IN English            
+        """
     }
 }
+
+private infix fun String.shouldBeAfterTrimming(expected: String) =
+    trimIndent().trim() shouldBe expected.trimIndent().trim()
