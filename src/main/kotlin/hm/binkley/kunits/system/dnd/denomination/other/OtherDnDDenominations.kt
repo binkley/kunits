d9f6fb06427c0e4eba4ebcf188d627cdf56d3e5b
Brian Oxley <boxley@thoughtworks.com>
2022-03-22 16:17:01 -0500

    Just one more

--- src/main/kotlin/hm/binkley/kunits/system/dnd/denomination/other/OtherDnDDenominations.kt
+++ src/main/kotlin/hm/binkley/kunits/system/dnd/denomination/other/OtherDnDDenominations.kt
@@ -3,11 +3,27 @@ package hm.binkley.kunits.system.dnd.denomination.other
 import hm.binkley.kunits.system.dnd.denomination.DnDDenomination
 import hm.binkley.kunits.system.dnd.denomination.DnDDenominations
 import hm.binkley.kunits.system.dnd.denomination.other.Adamant.AdamantPieces
+import hm.binkley.kunits.system.dnd.denomination.other.Bronze.BronzePieces
 import hm.binkley.kunits.system.dnd.denomination.other.Mithral.MithralPieces
 import hm.binkley.math.fixed.FixedBigRational
 import hm.binkley.math.fixed.over
 
 /** Example of adding to existing units of measure. */
+
+class Bronze private constructor(quantity: FixedBigRational) :
+    DnDDenomination<BronzePieces, Bronze>(BronzePieces, quantity) {
+    companion object BronzePieces : DnDDenominations<BronzePieces, Bronze>(
+        "bronze piece", 1 over 10
+    ) {
+        override fun new(quantity: FixedBigRational) = Bronze(quantity)
+        override fun format(quantity: FixedBigRational) = "$quantity bp"
+    }
+}
+
+val FixedBigRational.bronze get() = BronzePieces.new(this)
+val Long.bronze get() = (this over 1).bronze
+val Int.bronze get() = (this over 1).bronze
+
 class Mithral private constructor(quantity: FixedBigRational) :
     DnDDenomination<MithralPieces, Mithral>(MithralPieces, quantity) {
     companion object MithralPieces : DnDDenominations<MithralPieces, Mithral>(
--- src/test/kotlin/hm/binkley/kunits/system/dnd/denomination/other/OtherDnDDenominationsTest.kt
+++ src/test/kotlin/hm/binkley/kunits/system/dnd/denomination/other/OtherDnDDenominationsTest.kt
@@ -1,21 +1,27 @@
 package hm.binkley.kunits.system.dnd.denomination.other
 
 import hm.binkley.kunits.into
+import hm.binkley.kunits.system.dnd.denomination.copper
 import hm.binkley.kunits.system.dnd.denomination.other.Adamant.AdamantPieces
+import hm.binkley.kunits.system.dnd.denomination.other.Bronze.BronzePieces
 import hm.binkley.kunits.system.dnd.denomination.other.Mithral.MithralPieces
 import hm.binkley.kunits.system.dnd.denomination.platinum
+import hm.binkley.math.fixed.over
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 internal class OtherDnDDenominationsTest {
     @Test
     fun `should convert`() {
+        1L.bronze shouldBe ((1 over 10).copper into Bronze)
         1L.mithral shouldBe (10.platinum into Mithral)
         1L.adamant shouldBe (10.mithral into Adamant)
     }
 
     @Test
     fun `should pretty print`() {
+        "$BronzePieces" shouldBe "D&D denomination: bronze piece"
+        "${1.bronze}" shouldBe "1 bp"
         "$MithralPieces" shouldBe "D&D denomination: mithral piece"
         "${1.mithral}" shouldBe "1 mp"
         "$AdamantPieces" shouldBe "D&D denomination: adamant piece"
