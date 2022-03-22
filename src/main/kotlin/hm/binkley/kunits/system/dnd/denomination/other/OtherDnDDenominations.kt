5f2053a07021b47fd7808b415519ac5192f751b8
Brian Oxley <boxley@thoughtworks.com>
2022-03-22 16:08:18 -0500

    Finish alternate D&D coins

--- src/main/kotlin/hm/binkley/kunits/system/dnd/denomination/other/OtherDnDDenominations.kt
+++ src/main/kotlin/hm/binkley/kunits/system/dnd/denomination/other/OtherDnDDenominations.kt
@@ -2,6 +2,7 @@ package hm.binkley.kunits.system.dnd.denomination.other
 
 import hm.binkley.kunits.system.dnd.denomination.DnDDenomination
 import hm.binkley.kunits.system.dnd.denomination.DnDDenominations
+import hm.binkley.kunits.system.dnd.denomination.other.Adamant.AdamantPieces
 import hm.binkley.kunits.system.dnd.denomination.other.Mithral.MithralPieces
 import hm.binkley.math.fixed.FixedBigRational
 import hm.binkley.math.fixed.over
@@ -20,3 +21,17 @@ class Mithral private constructor(quantity: FixedBigRational) :
 val FixedBigRational.mithral get() = MithralPieces.new(this)
 val Long.mithral get() = (this over 1).mithral
 val Int.mithral get() = (this over 1).mithral
+
+class Adamant private constructor(quantity: FixedBigRational) :
+    DnDDenomination<AdamantPieces, Adamant>(AdamantPieces, quantity) {
+    companion object AdamantPieces : DnDDenominations<AdamantPieces, Adamant>(
+        "adamant piece", 100_000 over 1
+    ) {
+        override fun new(quantity: FixedBigRational) = Adamant(quantity)
+        override fun format(quantity: FixedBigRational) = "$quantity ap"
+    }
+}
+
+val FixedBigRational.adamant get() = AdamantPieces.new(this)
+val Long.adamant get() = (this over 1).adamant
+val Int.adamant get() = (this over 1).adamant
--- src/test/kotlin/hm/binkley/kunits/system/dnd/denomination/other/OtherDnDDenominationsTest.kt
+++ src/test/kotlin/hm/binkley/kunits/system/dnd/denomination/other/OtherDnDDenominationsTest.kt
@@ -1,6 +1,7 @@
 package hm.binkley.kunits.system.dnd.denomination.other
 
 import hm.binkley.kunits.into
+import hm.binkley.kunits.system.dnd.denomination.other.Adamant.AdamantPieces
 import hm.binkley.kunits.system.dnd.denomination.other.Mithral.MithralPieces
 import hm.binkley.kunits.system.dnd.denomination.platinum
 import io.kotest.matchers.shouldBe
@@ -10,11 +11,14 @@ internal class OtherDnDDenominationsTest {
     @Test
     fun `should convert`() {
         1L.mithral shouldBe (10.platinum into Mithral)
+        1L.adamant shouldBe (10.mithral into Adamant)
     }
 
     @Test
     fun `should pretty print`() {
         "$MithralPieces" shouldBe "D&D denomination: mithral piece"
         "${1.mithral}" shouldBe "1 mp"
+        "$AdamantPieces" shouldBe "D&D denomination: adamant piece"
+        "${1.adamant}" shouldBe "1 ap"
     }
 }
