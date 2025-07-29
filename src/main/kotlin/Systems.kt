07b534e0a55b4599cfad5205b768866f70be06da
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2025-07-29 16:29:26 -0500

    Lancre and Ankh-Morpork trade

--- src/main/kotlin/Systems.kt
+++ src/main/kotlin/Systems.kt
@@ -11,5 +11,13 @@ val FORTNIGHTS_PER_SECOND = 1 over 1_209_600
 /** Conversion from FFF to English for standard units of weight. */
 val FIRKINS_PER_DRAM = 1 over 23_040
 
+/**
+ * Conversion from Lancre pennies to Ankh-Morpork dollars in the Disword.
+ * Note that:
+ * - 240 pence to the dollar in Ankh-Morpork
+ * - 100 Lancre pennies to the Ankh-Morpork dollar
+ */
+val ANKH_MORPORK_PENCE_PER_LANCRE_PENNY = 100 over 240
+
 /** Conversion from English to MIT for standard units of length. */
 val SMOOTS_PER_LINE = 1 over 804
--- src/test/kotlin/system/discworld/lancre/LancreTest.kt
+++ src/test/kotlin/system/discworld/lancre/LancreTest.kt
@@ -1,5 +1,8 @@
 package hm.binkley.kunits.system.discworld.lancre
 
+import hm.binkley.kunits.system.discworld.ankhMorpork.denomination.Dollar.Dollars
+import hm.binkley.kunits.system.discworld.ankhMorpork.denomination.dollars
+import hm.binkley.kunits.system.discworld.lancre.denomination.pennies
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -7,5 +10,10 @@ internal class LancreTest {
     @Test
     fun `should pretty print`() {
         "$Lancre" shouldBe "Lancre"
+    }
+
+    @Test
+    fun `should convert into Ankh-Morpork`() {
+        (100.pennies intoAnkhMorpork Dollars) shouldBe 1.dollars
     }
 }
