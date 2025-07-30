dab33aaa650ae8299ec1cf43aea4458d0acc46cb
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2025-07-30 13:14:27 -0500

    Introduce Iron Pieces and Steel Pieces

--- src/main/kotlin/system/dnd/denomination/other/OtherDnDDenominations.kt
+++ src/main/kotlin/system/dnd/denomination/other/OtherDnDDenominations.kt
@@ -4,7 +4,9 @@ import hm.binkley.kunits.system.dnd.denomination.DnDDenomination
 import hm.binkley.kunits.system.dnd.denomination.DnDDenominations
 import hm.binkley.kunits.system.dnd.denomination.other.AdamantPiece.AdamantPieces
 import hm.binkley.kunits.system.dnd.denomination.other.BronzePiece.BronzePieces
+import hm.binkley.kunits.system.dnd.denomination.other.IronPiece.IronPieces
 import hm.binkley.kunits.system.dnd.denomination.other.MithrilPiece.MithrilPieces
+import hm.binkley.kunits.system.dnd.denomination.other.SteelPiece.SteelPieces
 import hm.binkley.math.fixed.FixedBigRational
 import hm.binkley.math.fixed.over
 
@@ -12,6 +14,21 @@ import hm.binkley.math.fixed.over
  * Examples of adding to existing units of measure.
  * See https://www.sageadvice.eu/prices-for-brass-steel-mithril-adamantine/.
  */
+
+class IronPiece private constructor(quantity: FixedBigRational) :
+    DnDDenomination<IronPieces, IronPiece>(IronPieces, quantity) {
+    companion object IronPieces : DnDDenominations<IronPieces, IronPiece>(
+        "Iron Piece",
+        1 over 100
+    ) {
+        override fun new(quantity: FixedBigRational) = IronPiece(quantity)
+        override fun format(quantity: FixedBigRational) = "$quantity IP"
+    }
+}
+
+val FixedBigRational.ironPieces get() = IronPieces.new(this)
+val Long.ironPieces get() = (this over 1).ironPieces
+val Int.ironPieces get() = (this over 1).ironPieces
 
 class BronzePiece private constructor(quantity: FixedBigRational) :
     DnDDenomination<BronzePieces, BronzePiece>(BronzePieces, quantity) {
@@ -29,6 +46,21 @@ class BronzePiece private constructor(quantity: FixedBigRational) :
 val FixedBigRational.bronzePieces get() = BronzePieces.new(this)
 val Long.bronzePieces get() = (this over 1).bronzePieces
 val Int.bronzePieces get() = (this over 1).bronzePieces
+
+class SteelPiece private constructor(quantity: FixedBigRational) :
+    DnDDenomination<SteelPieces, SteelPiece>(SteelPieces, quantity) {
+    companion object SteelPieces : DnDDenominations<SteelPieces, SteelPiece>(
+        "Steel Piece",
+        50 over 1
+    ) {
+        override fun new(quantity: FixedBigRational) = SteelPiece(quantity)
+        override fun format(quantity: FixedBigRational) = "$quantity TP"
+    }
+}
+
+val FixedBigRational.steelPieces get() = SteelPieces.new(this)
+val Long.steelPieces get() = (this over 1).steelPieces
+val Int.steelPieces get() = (this over 1).steelPieces
 
 class MithrilPiece private constructor(quantity: FixedBigRational) :
     DnDDenomination<MithrilPieces, MithrilPiece>(MithrilPieces, quantity) {
--- src/test/kotlin/system/dnd/denomination/other/OtherDnDDenominationsTest.kt
+++ src/test/kotlin/system/dnd/denomination/other/OtherDnDDenominationsTest.kt
@@ -2,9 +2,12 @@ package hm.binkley.kunits.system.dnd.denomination.other
 
 import hm.binkley.kunits.into
 import hm.binkley.kunits.system.dnd.denomination.copperPieces
+import hm.binkley.kunits.system.dnd.denomination.electrumPieces
 import hm.binkley.kunits.system.dnd.denomination.other.AdamantPiece.AdamantPieces
 import hm.binkley.kunits.system.dnd.denomination.other.BronzePiece.BronzePieces
+import hm.binkley.kunits.system.dnd.denomination.other.IronPiece.IronPieces
 import hm.binkley.kunits.system.dnd.denomination.other.MithrilPiece.MithrilPieces
+import hm.binkley.kunits.system.dnd.denomination.other.SteelPiece.SteelPieces
 import hm.binkley.kunits.system.dnd.denomination.platinumPieces
 import hm.binkley.math.fixed.over
 import io.kotest.matchers.shouldBe
@@ -13,15 +16,21 @@ import org.junit.jupiter.api.Test
 internal class OtherDnDDenominationsTest {
     @Test
     fun `should convert`() {
-        1L.bronzePieces shouldBe ((1 over 10).copperPieces into BronzePieces)
+        1L.ironPieces shouldBe ((1 over 100).copperPieces into IronPieces)
+        1L.bronzePieces shouldBe (10.ironPieces into BronzePieces)
+        1L.steelPieces shouldBe (1.electrumPieces into SteelPieces)
         1L.mithrilPieces shouldBe (10.platinumPieces into MithrilPieces)
         1L.adamantPieces shouldBe (10.mithrilPieces into AdamantPieces)
     }
 
     @Test
     fun `should pretty print`() {
+        "$IronPieces" shouldBe "D&D denomination: Iron Piece"
+        "${1.ironPieces}" shouldBe "1 IP"
         "$BronzePieces" shouldBe "D&D denomination: Bronze Piece"
         "${1.bronzePieces}" shouldBe "1 BP"
+        "$SteelPieces" shouldBe "D&D denomination: Steel Piece"
+        "${1.steelPieces}" shouldBe "1 TP"
         "$MithrilPieces" shouldBe "D&D denomination: Mithril Piece"
         "${1.mithrilPieces}" shouldBe "1 MP"
         "$AdamantPieces" shouldBe "D&D denomination: Adamant Piece"
